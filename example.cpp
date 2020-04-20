cf4feef6ff90e8e047e95187540cae735e9a696e
Cryst4l <bhalimi@outlook.fr>
2020-04-20 11:55:04 +0200

    add an example

--- example.cpp
+++ example.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
-#include "src/Tensor1D.h"
-#include "src/Tensor2D.h"
+#include "Eigen/Core"
+#include "src/Layer.h"
+#include "src/Activation.h"
+
+using namespace Eigen;
+using namespace Backprop;
+
 int main(void)
 {
-	Tensor1D A(10); A.setValue(5);
-	Tensor1D B(10); B.setValue(3);
-	int temp = dot(A,B);
-	std::cout << temp << '\n';
-
-	Tensor2D C(5,4); C.setValue(5);
-	Tensor2D D(5,4); D.setValue(3);
 /*
-	Tensor2D E = C-D;
-	for (int i = 0; i < E.rows(); i++) {
-		for (int j = 0; j < E.cols(); j++) {
-			std::cout << E(i,j) << ' ';
-		}
-		std::cout << std::endl; 
-	}
+	VectorXd a(5);
+	VectorXd b(5);
+	a << 1., 2., 3., 4., 5.;
+	b = a;
+	MatrixXd m = b * a.transpose();
+	std::cout << m << std::endl;
 */
+	//////////////////////////// 
+
+	VectorXd a(5);
+	a << 1., 2., 3., 4., 5.;
+	Activation f(5, Activation::SIGM);
+	a = f.propagate(a);
+	std::cout << a << std::endl;
+
+	///////////////////////////
+
+	VectorXd a(5);
+	a << 1., 2., 3., 4., 5.;
+
 	return 0;
 }
